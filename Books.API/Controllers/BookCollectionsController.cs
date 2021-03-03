7f6840f1bef25c727a23a7ae17d9a98820f86c68
Qing Ma <gsmofgsm@hotmail.com>
2021-03-03 21:28:23 +0100

    Create a collection books

---
+++ Books.API/Controllers/BookCollectionsController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Books.API.Models;
+using Books.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.API.Controllers
+{
+    [Route("api/bookcollections")]
+    [ApiController]
+    public class BookCollectionsController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        private readonly IMapper _mapper;
+
+        public BookCollectionsController(IBookRepository bookRepository, IMapper mapper)
+        {
+            _bookRepository = bookRepository ?? 
+                throw new ArgumentNullException(nameof(bookRepository));
+            _mapper = mapper ??
+                throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBookCollection(
+            IEnumerable<BookForCreation> bookCollection)
+        {
+            var bookEntities = _mapper.Map<IEnumerable<Entities.Book>>(bookCollection);
+
+            foreach (var bookEntity in bookEntities)
+            {
+                _bookRepository.AddBook(bookEntity);
+            }
+
+            await _bookRepository.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
